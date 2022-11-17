95093be0d71a28c7bb404635fd432751f8d6e098
Helder Fonseca <helder2391@gmail.com>
2022-11-17 03:33:02 -0100

    change page title

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   return (
           <>
             <Head>
-              <title>Email Marketing</title>
+              <title>Yellow Connect - Cabo Verde</title>
             </Head>
             <Component {...pageProps} />
           </>
